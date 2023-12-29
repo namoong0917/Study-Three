b69ebee3db3e4c07abb399e1621d5636de7f619e
namoong0917 <namoong0917@gmail.com>
2023-12-29 18:44:46 +0900

    기본 구성 요소 (Scene, Camera, Renderer)

--- src/js/index.js
+++ src/js/index.js
@@ -1,39 +1,40 @@
 import * as THREE from "three";
-import WebGL from "../../node_modules/three/examples/jsm/capabilities/WebGL.js";
 
-if (WebGL.isWebGLAvailable()) {
-  // 장면 생성
-  const scene = new THREE.Scene();
-  scene.background = new THREE.Color(0xffe187);
+const $result = document.getElementById("result");
 
-  const camera = new THREE.PerspectiveCamera(
-    50,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-  );
-  camera.position.set(3, 3, 3);
-  camera.lookAt(0, 0, 0);
+// 1. Scene: 화면에서 보여주려는 객체를 담는 공간
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xffe287);
+// scene.add(요소);
 
-  const renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
+// 2. Camera: Scene을 바라볼 시점을 결정
+const camera = new THREE.PerspectiveCamera(
+  50, // fov : 시야각, 커질 수록 화면에 많은 영역을 출력 기본값 50,사람의 시야와 유사한 45~75 사이 값 사용
+  $result.clientWidth / $result.clientHeight, // aspect 카메라의 종횡비
+  0.1, // near 출력되는 범위
+  1000 // far 출력되는 범위
+);
+camera.position.set(2, 2, 2); // 객체 위치 (x, y, z)
+camera.lookAt(0, 0, 0); // 바라볼 좌푝값 (x, y, z)
 
-  // 빛
-  const ambientLight = new THREE.AmbientLight(0xffffff, 1);
-  scene.add(ambientLight);
+// 3. Renderer: Scene + Camera, 화면을 그려주는 역할
+// 원하는곳에 canvas를 출력하기위해 WebGLRenderer() 안에 속성값으로
+// { canvas: 선택한 돔 요소 } 를 넣어준다.
+// antialias: true : 계단현상 안티엘리징
+const renderer = new THREE.WebGLRenderer({ canvas: $result, antialias: true });
+renderer.setSize($result.clientWidth, $result.clientHeight);
+// console.log(renderer);
+// document.body.appendChild(renderer.domElement);
 
-  const pointLight = new THREE.PointLight(0xffffff, 1);
-  pointLight.position.set(0, 2, 4);
-  scene.add(pointLight);
+// 빛 추가
+const light = new THREE.DirectionalLight(0xffffff);
+light.position.set(2, 4, 3);
+scene.add(light);
 
-  // 박스
-  const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const material = new THREE.MeshStandardMaterial({ color: 0x2e6ff2 });
-  const cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
-
-  renderer.render(scene, camera);
-} else {
-  document.body.appendChild(WebGL.getWebGLErrorMessage());
-}
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshStandardMaterial({
+  color: 0x2e6ff2,
+});
+const box = new THREE.Mesh(geometry, material);
+scene.add(box); // 화면에 box 보여주기
+renderer.render(scene, camera);
